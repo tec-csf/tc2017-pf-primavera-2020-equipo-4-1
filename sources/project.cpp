ae41a25638b58eab096054c9818671b77c955d1c
Isaac Garza <isaacgarza@MacBook-Pro-de-Isaac.local>
2020-05-17 19:49:26 -0500

    Se creo struct procesadores, inserta datos

---
+++ sources/project.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct processor
+{
+  int ID;
+  int cores;
+  //Probabilty of failure
+  float failureP;
+  //Probabilty of RC
+  float probRc;
+  //Probabilty of Rb
+  float probRb;
+  //Time of rc
+  float TRc;
+  //Time of rb
+  float TRb;
+  //Time of repair
+  float TRepair;
+  //1 = working, 0 = not working
+  int rc = 1; 
+  int rb = 1;
+  int failure = 1; 
+};
+
+void insert(processor *arr, int n, int cores, float alpha, float beta, float delta, float gamma, float c)
+{
+  for (int i = 0; i < n; i++)
+  {
+    arr[i].ID = i;
+    arr[i].cores = cores;
+    arr[i].failureP = 1/gamma;
+    arr[i].probRc = c;
+    arr[i].probRb = 1 - c; 
+    arr[i].TRc = 1/beta;
+    arr[i].TRb = 1/alpha;
+    arr[i].TRepair = 1/delta;
+  }
+}
+
+int main(){
+  int n = 3;
+  //pedir usuario antes el n (procesadores)
+  processor p[n];
+  insert(p,n,3,4,5,6,7,.9);
+
+  for (int i = 0; i < n; i++)
+  {
+    printf("Time of RC %f\n", p[i].TRb);
+  }
+  
+  return 0;
+}
