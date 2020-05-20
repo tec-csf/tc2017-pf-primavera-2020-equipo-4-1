b86a9c7959e84e4a86895095e9f555e42be05e18
Isaac Garza <isaacgarza@MacBook-Pro-de-Isaac.local>
2020-05-19 21:17:18 -0500

    php inserta datos en cpp

--- api/project.cpp
+++ api/project.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdlib.h>  
 
 using namespace std;
 
@@ -37,30 +38,50 @@ struct processor
 
 void insert(processor *arr, int n, int cores, float alpha, float beta, float delta, float gamma, float c)
 {
-  for (int i = 0; i < n; i++)
-  {
-    arr[i].ID = i;
-    arr[i].cores = cores;
-    arr[i].failureP = 1/gamma;
-    arr[i].probRc = c;
-    arr[i].probRb = 1 - c; 
-    arr[i].TRc = 1/beta;
-    arr[i].TRb = 1/alpha;
-    arr[i].TRepair = 1/delta;
+    for (int i = 0; i < n; i++)
+    {
+      arr[i].ID = i;
+      arr[i].cores = cores;
+      arr[i].failureP = 1/gamma;
+      arr[i].probRc = c;
+      arr[i].probRb = 1 - c; 
+      arr[i].TRc = 1/beta;
+      arr[i].TRb = 1/alpha;
+      arr[i].TRepair = 1/delta;
+    }
   }
-}
 
-int main(){
-  int n = 3;
-  //pedir usuario antes el n (procesadores)
-  processor p[n];
-  insert(p,n,3,4,5,6,7,.9);
+  int main(int argc, char *argv[] ){
+    if (argc != 8)
+    {
+        int n = atoi(argv[6]);
+    int cores = atoi(argv[7]);
+    float alpha = atoi(argv[2]);
+    float beta = atoi(argv[3]);
+    float delta = atoi(argv[4]);
+    float gamma = atoi(argv[5]);
+    float c = atoi(argv[8]);
 
-  for (int i = 0; i < n; i++)
-  {
-    cout << "Time of RC: " << p[i].TRb << endl;
-    //printf("Time of RC %f\n", p[i].TRb);
+    float xPoisson = atoi(argv[1]);
+    //pedir usuario antes el n (procesadores)
+    processor p[n];
+    insert(p,n,cores,alpha,beta,delta,gamma,c);
+
+    for (int i = 0; i < n; i++)
+    {
+      cout << "Nucleos: " << p[i].cores << endl;
+      cout << "Probabilidad de fallo: " << p[i].failureP << endl;
+      cout << "Probabilidad de RC: " << p[i].probRc << endl;
+      cout << "Probabilidad de RB: " << p[i].probRb << endl;
+      cout << "Tiempo de RC: " << p[i].TRc << endl;
+      cout << "Tiempo de RB: " << p[i].TRb << endl;
+      cout << "Tiempo de Reparacion: " << p[i].TRc << endl;
+    }
+  }else{
+    cout<<"ERROR: Te falto un dato"<<endl;
   }
+  
+  
   
   return 0;
 }
--- frontend/elements-sility-vcard-cv-resume-html-template-E6LGKK-pmdnc9gX-03-27 (1)/sility/scripts/contact.php
+++ frontend/elements-sility-vcard-cv-resume-html-template-E6LGKK-pmdnc9gX-03-27 (1)/sility/scripts/contact.php
@@ -1,8 +1,18 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if(!empty($_POST['x']) && !empty($_POST['alfa']) && !empty($_POST['beta']) && !empty($_POST['delta']) && !empty($_POST['gamma']) && !empty($_POST['process']) && !empty($_POST['nuc']) && !empty($_POST['c'])) {
-		$output = shell_exec('./pro');
-		echo "<h1>$output</h1>";
+		$x = $_POST['x'];
+		$alfa = $_POST['alfa'];
+		$beta = $_POST['beta'];
+		$delta = $_POST['delta'];
+		$gamma = $_POST['gamma'];
+		$process = $_POST['process'];
+		$nuc = $_POST['nuc'];
+		$c = $_POST['c'];
+		$output = shell_exec("./pro $x $alfa $beta $delta $gamma $process $nuc $c");
+		echo "<h4>$output</h4>";
+	}else {
+		echo "<h4>FALTO UN DATO</h4>";
 	}
 }
 ?>
