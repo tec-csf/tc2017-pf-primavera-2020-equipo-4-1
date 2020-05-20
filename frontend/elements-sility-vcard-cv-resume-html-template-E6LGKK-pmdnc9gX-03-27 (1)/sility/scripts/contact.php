6a91dbf1de914b631e9c96bd13041182af831da4
Isaac Garza <isaacgarza@MacBook-Pro-de-Isaac.local>
2020-05-19 20:14:47 -0500

    PHP working

--- frontend/elements-sility-vcard-cv-resume-html-template-E6LGKK-pmdnc9gX-03-27 (1)/sility/scripts/contact.php
+++ frontend/elements-sility-vcard-cv-resume-html-template-E6LGKK-pmdnc9gX-03-27 (1)/sility/scripts/contact.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if(!empty($_POST['x']) && !empty($_POST['alfa']) && !empty($_POST['beta']) && !empty($_POST['delta']) && !empty($_POST['gamma']) && !empty($_POST['process']) && !empty($_POST['nuc']) && !empty($_POST['c'])) {
-		exec('./pro.exe', $out);
+		$output = shell_exec('./pro');
+		echo "<h1>$output</h1>";
 	}
 }
 ?>
