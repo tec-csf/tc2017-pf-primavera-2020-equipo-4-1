dae53049e03de130094af4f5988ac24e487cb3e4
RodrigoSibaja <42215175+RodrigoSibaja@users.noreply.github.com>
2020-05-22 16:25:58 -0500

    Utiliza deque, ya no queue (falta arreglar emparejamiento)

--- api/MultiplicadorMatrices.cpp
+++ api/MultiplicadorMatrices.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <deque>
 #include <time.h>
 using namespace std;
+
+/* En memoria de:
+ * vector<tuple<vector<vector<int>>,int>>
+ */ 
 
 vector<vector<int>> generarMatriz(int n, int m)
 {
@@ -26,29 +30,62 @@ vector<vector<int>> generarMatriz(int n, int m)
     return vec;
 }
 
-void emparejar(queue<vector<vector<int>>> lista)
+void multiplicar(vector<vector<int>> A, vector<vector<int>> B)
 {
+    vector<vector<int>> C(A.size(), vector<int> (B[0].size(),0));
+    for(int i=0; i<A.size(); i++)
+    {
+        for(int j=0; j<B[0].size(); j++)
+        {
+            for(int k=0; k<A[0].size(); k++) 
+            {
+                C[i][j]+=A[i][k]*B[k][j];
+            }
+        }
+    }
+    for (int i = 0; i < C.size(); i++) { 
+        for (int j = 0; j < C[0].size(); j++){ 
+            cout<< C[i][j]<< " "; 
+        } 
+        cout<< "\n"; 
+    }
+}
+
+void emparejar(deque<vector<vector<int>>> lista)
+{
+    lista.shrink_to_fit();
     if (lista.front()[0].size()==lista.back().size())
     {
-        cout<<"La primera y última matriz de la cola son compatibles"<<endl;
+        if(lista.size()>1)
+        {
+
+            cout<<"La primera y última matriz de la cola son compatibles"<<endl;
+            multiplicar(lista.front(), lista.back());
+            lista.pop_front(); lista.pop_back();
+            lista.shrink_to_fit();
+            cout<<"COLA: "<<lista.size()<<endl<<endl; 
+        }
     }
     else
     {
+        lista.shrink_to_fit();
         cout<<"NO hay matrices compatibles"<<endl;
+        cout<<"COLA: "<<lista.size()<<endl<<endl;
     }
 }
 
 int main()
 {
-    //vector<tuple<vector<vector<int>>,int>> list;
-    queue<vector<vector<int>>> list;
+    deque<vector<vector<int>>> list;
 
     srand(time(NULL));
 
-    list.push(generarMatriz(rand() % 4 + 2,(rand() % 4 + 2)));
-    list.push(generarMatriz(rand() % 4 + 2,(rand() % 4 + 2)));
-
-    emparejar(list);
+    for(int i = 0; i < 5; i++)
+    {
+        list.shrink_to_fit();
+        list.push_back(generarMatriz(rand() % 4 + 2,(rand() % 4 + 2)));
+        emparejar(list);
+    }
 
     return 0;
 }
