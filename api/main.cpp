733a1a0c47c08980bfd31998801f1f142fb8a2e6
sergio-hernandez-castillo <a01025210@itesm.mx>
2020-05-24 18:54:11 -0600

    Currently stuck at pthread_create

--- api/main.cpp
+++ api/main.cpp
@@ -1,17 +1,68 @@
-#include "Processor.hpp"
+//#include "Processor.hpp"
 #include "Matrices.hpp"
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <time.h>
+#include <pthread.h>
+#include <omp.h>
+#include <limits>
+#include <math.h>
 
 using namespace std;
 
+struct arg_struct {
+    deque<vector<vector<int>>>::iterator arg1;
+    deque<vector<vector<int>>>::iterator arg2;
+};
+
+struct thread {
+    pthread_t tid;
+    bool isWorking = false;
+};
+
+class Processor {
+    private:
+    int id, cores, rc = 1, rb = 1, failure = 1;
+    float failureP, probRc, probRb, TRc, TRb, TRepair;
+
+    public:
+    void setEverything(int anID, int amountOfCores, float alpha, float beta,
+                    float delta, float gamma, float c);
+    int calculatePoisson(int x);
+};
+
+void Processor::setEverything(int anID, int amountOfCores, float alpha, 
+                                float beta, float delta, float gamma, float c)
+{
+    id = anID;
+    cores = amountOfCores;
+    failureP = 1 / gamma;
+    probRc = c;
+    probRb = 1 - c;
+    TRc = 1 / beta;
+    TRb = 1 / alpha;
+    TRepair = 1 / delta;
+}
+
+int Processor::calculatePoisson(int x){
+    srand(time(0));
+    float r = rand() / INT_MAX;
+    int xPoisson = (1 / x) * log(1 - r);
+
+    return xPoisson;
+}
+
 int main(int argc, char *argv[]){
-    int amountOfProcessors = atoi(argv[1]), amountOfCores = atoi(argv[2]);
+    int amountOfProcessors = atoi(argv[1]), amountOfCores = atoi(argv[2]), i;
     float alpha = atof(argv[3]), beta = atof(argv[4]), delta = atof(argv[5]), 
-        gamma = atof(argv[6]), c = atof(argv[7]);
-    int i;
+            gamma = atof(argv[6]), c = atof(argv[7]);
+    bool canTheyMultiply = false;
+    struct arg_struct args;
 
     srand(time(NULL));
 
-    vector<pthread_t> threads(amountOfProcessors);
+    vector<thread> listOfThreads(amountOfProcessors);
     vector<Processor> processors(amountOfProcessors);
 
     for (i = 0; i < amountOfProcessors; i++){
@@ -21,7 +72,28 @@ int main(int argc, char *argv[]){
     while(true){
         list.shrink_to_fit();
         list.push_back(generarMatriz(rand() % 4 + 2, (rand() % 4 + 2)));
-        emparejar();
+        canTheyMultiply = emparejar();
+
+        if (canTheyMultiply == true){
+            cout << "COLA: " << list.size() << endl;
+
+            for (i = 0; i < amountOfProcessors; i++){
+                args.arg1 = list.front();
+                args.arg2 = list.back();
+                pthread_create(&listOfThreads[i].tid, NULL, multiplicar(list.front(), list.back()), (void *)i);
+            }
+
+            multiplicar(list.front(), list.back());
+            list.pop_front();
+            list.pop_back();
+            list.shrink_to_fit();
+        }
+
+        /*
+        else {
+            cout << "The matrices were not compatible." << endl;
+        }
+        */
     }
 
     return 0;
