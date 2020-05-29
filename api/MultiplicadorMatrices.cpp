b9c7a0cd9c04772747fb74727167e40aee1dcffc
RodrigoSibaja <42215175+RodrigoSibaja@users.noreply.github.com>
2020-05-28 21:33:15 -0500

    Empareja como pidió el profesor, utiliza vector en vez de deque y tiene OpenMP

--- api/MultiplicadorMatrices.cpp
+++ api/MultiplicadorMatrices.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <deque>
 #include <time.h>
-using namespace std;
+#include <omp.h>
+#include <limits.h>
+#include <math.h>
+#include <unistd.h>
 
+#define NCORES 3
+
+using namespace std;
 /* En memoria de:
  * vector<tuple<vector<vector<int>>,int>>
  */
-deque<vector<vector<int>>> list;
+
+vector<vector<vector<int>>> list;
 
 vector<vector<int>> generarMatriz(int n, int m)
 {
     vector<vector<int>> vec(n, vector<int> (m,0));
-    cout<<"filas: "<<vec.size()<<"\tcolumnas: "<<vec[0].size()<<endl;
+    cout<<"***filas: "<<vec.size()<<"\tcolumnas: "<<vec[0].size()<<endl;
     for (int i = 0; i < n; i++)
     {
         for(int j = 0; j < m; j++)
@@ -34,8 +40,11 @@ vector<vector<int>> generarMatriz(int n, int m)
 void multiplicar(vector<vector<int>> A, vector<vector<int>> B)
 {
     vector<vector<int>> C(A.size(), vector<int> (B[0].size(),0));
+    omp_set_num_threads(NCORES);
+    #pragma omp parallel for 
     for(int i=0; i<A.size(); i++)
     {
+        printf("hilo: %d\n", omp_get_thread_num());
         for(int j=0; j<B[0].size(); j++)
         {
             for(int k=0; k<A[0].size(); k++) 
@@ -54,19 +63,29 @@ void multiplicar(vector<vector<int>> A, vector<vector<int>> B)
 
 void emparejar()
 {
-    if (list.front()[0].size()==list.back().size())
+    bool flag = true;
+    for(int i = 0; i < list.size()-1; i++)
     {
-        if(list.size()>1)
+        cout<<list[i][0].size()<<" X "<<list.back().size()<<endl;
+        if (list[i][0].size()==list.back().size())
         {
-            cout<<"La primera y última matriz de la cola son compatibles"<<endl;
-            multiplicar(list.front(), list.back());
-            list.pop_front(); list.pop_back();
+            if(list.size()>1)
+            {
+                cout<<"La primera y última matriz de la cola son compatibles"<<endl;
+                multiplicar(list[i], list.back());
+                cout<<"multiplicó"<<endl;
+                list.erase(list.begin()+i); 
+                list.pop_back();
+                cout<<"eliminó"<<endl;
+                flag = false;
+                break;
+            }
         }
-    }
-    else
-    {
-        cout<<"NO hay matrices compatibles"<<endl;
-    }
+        else if (flag)
+        {
+            cout<<"NO hay matrices compatibles"<<endl;
+        }
+  }
         cout<<"COLA: "<<list.size()<<endl<<endl;
 }
 
@@ -76,10 +95,9 @@ int main()
 
     srand(time(NULL));
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 100; i++)
     {
-        list.shrink_to_fit();
-        list.push_back(generarMatriz(rand() % 4 + 2,(rand() % 4 + 2)));
+        list.push_back(generarMatriz(rand() % 8 + 2,(rand() % 8 + 2)));
         emparejar();
     }
 
