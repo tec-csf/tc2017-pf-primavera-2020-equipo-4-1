020cd8b2a55f78dad3568068b27af36c71589b22
Sergio Hernandez Castillo <shcastillo@Sergios-MBP.lan>
2020-05-30 13:01:45 -0500

    Updated header file

--- api/Matrices.hpp
+++ api/Matrices.hpp
@@ -1,6 +1,7 @@
 #ifndef MATRICES_HPP
 #define MATRICES_HPP
 
+#include "Processor.hpp"
 #include <iostream>
 #include <vector>
 #include <deque>
@@ -13,33 +14,28 @@ using namespace std;
 struct arg_struct {
     vector<vector<int>> *arg1;
     vector<vector<int>> *arg2;
+    vector<Processor> *arg3;
+    int *arg4;
 };
 
 vector<vector<int>> generarMatriz(int n, int m)
 {
-    vector<vector<int>> vec(n, vector<int>(m, 0));
-
-    //cout << "filas: " << vec.size() << "\tcolumnas: " << vec[0].size() << endl;
-
+    vector<vector<int>> vec(n, vector<int> (m,0));
+    // cout<<"***filas: "<<vec.size()<<"\tcolumnas: "<<vec[0].size()<<endl;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for(int j = 0; j < m; j++)
         {
             vec[i][j] = rand() % 9;
         }
     }
-
-    /*
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << vec[i][j] << " ";
-        }
-        cout << "\n";
+    for (int i = 0; i < n; i++) { 
+        for (int j = 0; j < m; j++){ 
+            cout<< vec[i][j]<< " "; 
+        } 
+        cout<< "\n"; 
     }
-    cout << "\n";
-    */
+    cout<< "\n"; 
 
     return vec;
 }
@@ -49,6 +45,8 @@ void * multiplicar(void * param)
     arg_struct *temp = (arg_struct *) param;
     vector<vector<int>> m1 = *(temp->arg1);
     vector<vector<int>> m2 = *(temp->arg2);
+    vector<Processor> processors = *(temp->arg3);
+    int id = *(temp->arg4);
     //vector<vector<int>> C(m1.size(), vector<int>(m2.size()));
 
     printf("I am M1\n");
@@ -73,6 +71,8 @@ void * multiplicar(void * param)
 
     printf("\n");
 
+    omp_set_num_threads(processors[id].getCores());
+    #pragma omp parallel for 
     for (int i = 0; i < m1.size(); i++)
     {
         for (int j = 0; j < m2[0].size(); j++)
@@ -89,15 +89,16 @@ void * multiplicar(void * param)
 
         cout << endl;
     }
+
+    pthread_exit();
 }
 
-bool emparejar(deque<vector<vector<int>>> list)
+bool emparejar(vector<vector<vector<int>>> list, int c)
 {
-    if (list.front()[0].size() == list.back().size())
+    if (list[c][0].size()==list.back().size())
     {
-        if (list.size() > 1)
+        if(list.size()>1)
         {
-            //cout << "La primera y última matriz de la cola son compatibles" << endl;
             return true;
         }
     }
